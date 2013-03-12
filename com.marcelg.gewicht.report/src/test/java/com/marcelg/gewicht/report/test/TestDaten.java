aa740c9ac2b3e41720b0de01a328a6c3330f2d5b
MrConjurer <MrConjurer@MOBILEBOX>
2013-03-12 21:11:09 +0100

    test init

---
+++ com.marcelg.gewicht.report/src/test/java/com/marcelg/gewicht/report/test/TestDaten.java
@@ -0,0 +1,20 @@
+package com.marcelg.gewicht.report.test;
+/**
+ * 
+ */
+
+/**
+ * @author MrConjurer
+ *
+ */
+public class TestDaten {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
