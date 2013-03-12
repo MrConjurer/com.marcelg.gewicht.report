f517bc85f4ec775bc753fce107ef409589dde1e4
MrConjurer <MrConjurer@MOBILEBOX>
2013-03-12 22:46:13 +0100

    init

--- com.marcelg.gewicht.report/src/main/java/com/marcelg/gewicht/report/data/User.java
+++ com.marcelg.gewicht.report/src/main/java/com/marcelg/gewicht/report/data/User.java
@@ -2,20 +2,26 @@ package com.marcelg.gewicht.report.data;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "USER")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private Long id;
 
+	@Column(name = "NAME")
 	private String name;
 
+	@Column(name = "ALTER")
 	private Integer alter;
 
 	private List<Weighing> weighings;
